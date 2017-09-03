0fb5edfd1b473ccd58ccf749b5b52464d106cabc
kamlesh <kamaleshs48@gmail.com>
2017-09-03 13:29:12 +0530

    kk

---
+++ HRMS/Repository/mySQLCommanFunction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
+using System.Data;
+namespace HRMS.Repository
+{
+    public class mySQLCommanFunction
+    {
+
+        public static bool Addd()
+        {
+            MySqlCommand sqlcom = new MySqlCommand();
+            sqlcom.CommandText = "INSERT INTO `tb_baobei` (`tb_Name`,`tb_Price`,`tb_Image`,`tb_Url`)" +
+                    " VALUES(@tb_Name,@tb_Price,@tb_Image,@tb_Url);";
+            MySqlParameter[] commandParameters = new MySqlParameter[]{
+                        new MySqlParameter("@tb_Name",""),
+                        new MySqlParameter("@tb_Price",""),
+                        new MySqlParameter("@tb_Image",""),
+                        new MySqlParameter("@tb_Url","")
+                      };
+            return MySqlHelper.ExecuteNonQuery(CommandType.StoredProcedure, MySqlHelper.ConnectionStringManager, sqlcom.CommandText, commandParameters);
+        }
+
+    }
+}
